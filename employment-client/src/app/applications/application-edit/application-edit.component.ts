6c5a4068d524c71242209ddaba809e369af02fe5
Madhav Mehta <madhavm16@gmail.com>
2019-01-17 22:51:18 -0600

    change component entirely

--- employment-client/src/app/applications/application-edit/application-edit.component.ts
+++ employment-client/src/app/applications/application-edit/application-edit.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Application } from 'src/app/models/application.model';
-import { ApplicationService} from 'src/app/service/application.service';
-import { FormBuilder, FormGroup, NgForm} from '@angular/forms';
-import { Router, ActivatedRoute } from "@angular/router";
-import {first} from "rxjs/operators";
+import { ApplicationService } from 'src/app/service/application.service';
+import { FormBuilder, FormGroup, NgForm } from '@angular/forms';
+import { Router } from "@angular/router";
+import { first } from "rxjs/operators";
 
 @Component({
   selector: 'app-application-edit',
@@ -15,52 +15,33 @@ export class ApplicationEditComponent implements OnInit {
 
   editForm: FormGroup;
   application: Application
-  _id: number;
-  companyName: string;
-  position: string;
-  positionType: string;
-  status: string;
-  farthestProcess: string;
-  dateApplied: string;
-  dateResponse: string;
-  portalUser: string;
-  portalPass: string;
-  sourceApplication: string;
-  isLoadingResults = false;
 
-  constructor(private fb: FormBuilder, private router: Router, private as: ApplicationService, private route: ActivatedRoute) {}
+  constructor(private fb: FormBuilder, private router: Router, private as: ApplicationService) {}
 
   ngOnInit() {
-    this.getApplication(this.route.snapshot.params['id']);
-    this.editForm = this.fb.group( {
-      companyName: [null],
-      position: [null],
-      positionType: [null],
-      status: [null],
-      farthestProcess: [null],
-      dateApplied: [null],
-      dateResponse: [null],
-      portalUser: [null],
-      portalPass: [null],
-      sourceApplication: [null]
-      });
-  }
+    let appId = localStorage.getItem("editApp");
+    if(!appId){
+      alert("Invalid action.")
+      this.router.navigate(['applications']);
+      return;
+    }
 
-  getApplication(id) {
-    this.as.getApplication(id).subscribe(data => {
-      this._id = data.id;
-      this.editForm.setValue( {
-        companyName: data.companyName,
-        position: data.position,
-        positionType: data.positionType,
-        status: data.status,
-        farthestProcess: data.farthestProcess,
-        dateApplied: data.dateApplied,
-        dateResponse: data.dateResponse,
-        portalUser: data.portalUser,
-        portalPass: data.portalPass,
-        sourceApplication: data.sourceApplication
-      });
+    this.editForm = this.fb.group({
+      id: [],
+      companyName: [''],
+      position: [''],
+      positionType: [''],
+      status: [''],
+      farthestProcess: [''],
+      dateApplied: [''],
+      dateResponse: [''],
+      portalUser: [''],
+      portalPass: [''],
+      sourceApplication: ['']
+    });
+
+    this.as.getApplication(+appId).subscribe( data => {
+      this.editForm.setValue(data);
     });
   }
 
